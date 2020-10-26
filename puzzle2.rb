323232e01d120180e7478d019bbd51c573ba6757
laurayon <59146632+laurayon@users.noreply.github.com>
2020-10-26 21:48:22 +0100

    Update puzzle2.rb

--- puzzle2.rb
+++ puzzle2.rb
@@ -1,23 +1,22 @@
 #we use puzzle1.rb as library, we use the order 'relative'
 #because puzzle1 is in the same directory as puzzle2. 
-require_relative 'puzzle1.rb'
+require_relative 'Puzzle1.rb'
 #we require the ruby GNOME2 library in order to work with graphic interfaces
 require 'gtk3'
+
 #we create a class puzzle2 that inherits from  Gtk::Window 
-class puzzle2 < Gtk::Window 
+class Puzzle2 < Gtk::Window 
   #we define the class constructor 
   def initialize 
     #Variable inicialization, all instance variables
     super #inherit from superclass
-    #css_provider is needed in gtk3 to change parameters
+    #css_provider can be used in gtk3 to store and change parameters
     #such as colour or font 
     @css_provider = Gtk::CssProvider.new
     #Create a grid to help widget placement 
     @grid = Gtk::Grid.new
     #Create a label 
     @label = Gtk::Label.new
-    #define label characteristics 
-    clearButton
     #Create 'Clear' button
     @cbutton = Gtk::Button.new(:label => "Clear")
     
@@ -38,6 +37,8 @@ class puzzle2 < Gtk::Window
     #Define initial disposition of button and label on the window
     #Attach label to grid: attach(child, column, row, width, height)
     @grid.attach @label, 0, 0, 1, 1
+    #Define label characteristics by calling clearButton method 
+    clearButton
     #Attach button to grid 
     @grid.attach @cbutton, 0, 1, 1, 1
     
@@ -46,6 +47,9 @@ class puzzle2 < Gtk::Window
     signal_connect "destroy" do Gtk.main_quit end 
     #Define consequence of clicking 'Clear' button 
     @cbutton.signal_connect "clicked" do clearButton end
+    
+    #Display window on screen
+    show_all 
   end  
   
   
@@ -74,41 +78,24 @@ class puzzle2 < Gtk::Window
   end 
 end 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Terminates the programm if the GUI cannot be initialized 
-Gtk.init 
-# Create a new window
-window = Gtk::Window.new Puzzle 2 Application 
-label = Gtk::Label.new You are a trout!
-window.add label
-window.signal_connect(destroy) { Gtk.main_quit }
-window.show_all
-Gtk.main
-
-#we set a thread exception in case thread doesn't work 
-Thread.abort_on_exception = true; 
-#start a thread for reading the uid 
-t_read = Thread.new {}
-//Ruby doesn't wait for threads to finish 
-
-t.join //to make thre thread finish 
+#MAIN PROGRAMM CODE
+if __FILE__ == $0
+  #Terminates the programm if the GUI cannot be initialized 
+  Gtk.init 
+  #we set a thread exception in case thread doesn't work 
+  Thread.abort_on_exception = true; 
+  #Create a new window
+  w = Puzzle2.new()
+  #Create object from Rfid class
+  rf = Rfid.new()
+  #Create a thread that allows reading from UID while graphic window is
+  #asking for UID 
+  t_read = Thread.new {
+    while 1 do 
+      w.aux(rf.read_uid())
+      sleep 1 
+    end
+    }
+  #Run main loop until main_quit is called 
+  Gtk.main
+end
