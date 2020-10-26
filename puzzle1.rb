2039814175632aca00859ccfc23150301f6daa89
laurayon <59146632+laurayon@users.noreply.github.com>
2020-10-26 16:31:19 +0100

    Update puzzle1.rb

--- puzzle1.rb
+++ puzzle1.rb
@@ -2,17 +2,17 @@
 #creamos una clase que se llama Rfid 
 class Rfid
 #definimos un método que se llama read_uid 
-	def read_uid
-			userid = gets.chomp #string without enter
-			#converts string data to decimal integer 
-			userid = userid.to_i
-			#converts decimal to hexadecimal string
-			userid = userid.to_s(16)
-			#converts to caps  
-			userid = userid.upcase
-		#ruby returns the last evaluated expression, so 
-		#we do not need to use 'return' here 
-	end
+  def read_uid
+    userid = gets.chomp #string without enter
+    #converts string data to decimal integer 
+    userid = userid.to_i
+    #converts decimal to hexadecimal string
+    userid = userid.to_s(16)
+    #converts to caps 
+    userid = userid.upcase
+    #ruby returns the last evaluated expression, so 
+    #we do not need to use 'return' here 
+  end
 end
 
 #En este caso lo estamos usando como ejecutable. 
