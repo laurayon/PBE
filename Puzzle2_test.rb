0aebfb336246aea14d5eb897c9ef72887c50c20e
laurayon <59146632+laurayon@users.noreply.github.com>
2020-10-27 09:04:23 +0100

    Update Puzzle2_test.rb

--- Puzzle2_test.rb
+++ Puzzle2_test.rb
@@ -4,63 +4,69 @@ require_relative 'Puzzle1.rb'
 #we require the ruby GNOME2 library in order to work with graphic interfaces
 require 'gtk3'
 
-#we create a class puzzle2 that inherits from  Gtk::Window 
-class Puzzle2 < Gtk::Window 
+#we create a class puzzle2 that inherits from  Gtk::ApplicationWindow 
+#it will create an Application that will gererate a window
+class Puzzle2 < Gtk::ApplicationWindow  
   #we define the class constructor 
   def initialize 
-    #Variable inicialization, all instance variables
-    super #inherit from superclass
-    #css_provider can be used in gtk3 to store and change parameters
-    #such as colour or font 
-    @css_provider = Gtk::CssProvider.new
-    #Create a grid to help widget placement 
-    @grid = Gtk::Grid.new
-    #Create a label 
-    @label = Gtk::Label.new
-    #Create 'Clear' button
-    @cbutton = Gtk::Button.new(:label => "Clear")
+    super("puzzle2.application", :flags_none) 
+    #Define activate event for application window
+    signal_connect "activate" do |application|
+      #Define Application elements 
+      #Create a window 
+      w = ExampleAppWindow.new(application)
+      #Create a grid to help widget placement on window
+      grid = Gtk::Grid.new
+      #Create 'Clear button'
+      cbutton = Gtk::Button.new(:label => "Clear")
+      #Create a label 
+      label = Gtk::Label.new
+      #Create css_provider (used in gtk3 to store and change parameters
+      #such as colour or font)  
+      css_provider = Gtk::CssProvider.new
+      
+      #Characterize elements 
+      #Set window tittle 
+      w.set_title "Puzzle 2 Application"
+      #Set window and border dimensions
+      w.set_size_request(550,200) 
+      w.set_border_width(10)
+      #set screen position where window is displayed: center
+      w.set_window_position(:center)
+      #Set label dimensions
+      label.set_size_request(550,150) 
+      #Set 'Clear' button dimensions 
+      cbutton.set_size_request(550,50) 
+      
+      #Define window interactions 
+      #Define consequence of clicking 'Clear' button 
+      cbutton.signal_connect ("clicked") {clearButton}
+      
+      #Aesthetically characterize window
+      #Add grid to window 
+      w.add(grid)
+      #Define initial disposition of button and label on the window
+      #Attach label to grid: grid cell (0,0), no spanning 
+      grid.attach (label, 0, 0, 1, 1) 
+      #Define label characteristics by calling clearButton method 
+      clearButton
+      #Attach button to grid : grid cell (0,1), no spanning 
+      grid.attach (cbutton, 0, 1, 1, 1)
+      
+      #Show all widgets on window 
+      window.show_all
+    end
+  end    
+end     
     
-    #Window definition
-    #Set window tittle 
-    set_title "Puzzle 2 Application"
-    #Set window and border dimensions
-    set_size_request(550,200) 
-    set_border_width(10)
-    #Set label dimensions
-    @label.set_size_request(550,150) 
-    #Set 'Clear' button dimensions 
-    @cbutton.set_size_request(550,50) 
-    #set screen position where window is displayed: center
-    set_window_position(:center)
-    #Add grid to window 
-    add(@grid) 
-    #Define initial disposition of button and label on the window
-    #Attach label to grid: attach(child, column, row, width, height)
-    @grid.attach @label, 0, 0, 1, 1
-    #Define label characteristics by calling clearButton method 
-    clearButton
-    #Attach button to grid 
-    @grid.attach @cbutton, 0, 1, 1, 1
-    
-    #Define window interactions 
-    #Define that window closes when 'x' button is pressed 
-    signal_connect "destroy" do Gtk.main_quit end 
-    #Define consequence of clicking 'Clear' button 
-    @cbutton.signal_connect "clicked" do clearButton end
-    
-    #Display window on screen
-    show_all 
-  end  
-  
-  
   #This function defines what happens when 'Clear' button is pressed 
   def clearButton
     #label restores message
-    @label.set_text "Please, login with your university card"
+    label.set_text "Please, login with your university card"
     #label turns blue, text in white
     css_provider.load(:data => "label {background-color: blue;}\
                                 label{color: white;}")
-    @label.style_context.add_provider(css_provider, Gtk::StyleProvider::PRIORITY_USER)
+    label.style_context.add_provider(css_provider, Gtk::StyleProvider::PRIORITY_USER)
   end 
   
   #This function defines what happens when UID is scanned 
@@ -68,9 +74,9 @@ class Puzzle2 < Gtk::Window
     #label turns red  
     css_provider.load(:data => "label {background-color: red;}\
                                 label{color: white;}")
-    @label.style_context.add_provider(css_provider, Gtk::StyleProvider::PRIORITY_USER)
+    label.style_context.add_provider(css_provider, Gtk::StyleProvider::PRIORITY_USER)
     #Message on label changes to 'uid: scanned UID'
-    @label.set_text "uid: #{string}"
+    label.set_text "uid: #{string}"
   end 
   
   #This function works for threads 
@@ -80,12 +86,12 @@ end
 
 #MAIN PROGRAMM CODE
 if __FILE__ == $0
-  #Terminates the programm if the GUI cannot be initialized 
-  Gtk.init 
   #we set a thread exception in case thread doesn't work 
   Thread.abort_on_exception = true; 
   #Create a new window
-  w = Puzzle2.new()
+  app = Puzzle2.new()
+  status = app.run([$0] + ARGV)
+  puts status
   #Create object from Rfid class
   rf = Rfid.new()
   #Create a thread that allows reading from UID while graphic window is
