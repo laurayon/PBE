fcb1b1d31d07d79154982933d5fd56adcce36b5b
laurayon <59146632+laurayon@users.noreply.github.com>
2020-10-27 12:39:16 +0100

    Update Puzzle1.rb

--- Puzzle1.rb
+++ Puzzle1.rb
@@ -1,16 +1,9 @@
 #return uid in hexa str 
-#creamos una clase que se llama Rfid 
 class Rfid
 #definimos un método que se llama read_uid 
   def read_uid
     userid = gets.chomp #string without enter
-    #converts string data to decimal integer 
-    userid = userid.to_i
-    #converts decimal to hexadecimal string
-    userid = userid.to_s(16)
-    #converts to caps 
-    userid = userid.upcase
-    #ruby returns the last evaluated expression, so 
-    #we do not need to use 'return' here 
+    #converts string to hexadecimal, caps string
+    userid = userid.to_i.to_s(16).upcase
   end
 end
