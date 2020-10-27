64af93cda005c9495b76f5f56e3955c95417baef
laurayon <59146632+laurayon@users.noreply.github.com>
2020-10-27 13:47:15 +0100

    Update Puzzle1.rb

--- Puzzle1.rb
+++ Puzzle1.rb
@@ -1,9 +1,9 @@
 #return uid in hexa str 
 class Rfid
 #definimos un método que se llama read_uid 
-  def read_uid
-    userid = gets.chomp #string without enter
+  def read_uid (String str)
     #converts string to hexadecimal, caps string
+    userid = str
     userid = userid.to_i.to_s(16).upcase
   end
 end
